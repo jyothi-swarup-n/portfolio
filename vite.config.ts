90ea3540ab1e71f7521cf8797427c4a282817dda
jyothi swarup <jyo-this-warup@jyothis-mac.local>
2026-03-27 12:09:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/",          // 👈 add this line
+  base: "/",          // 👈 add this line
 
   server: {
     host: "::",
